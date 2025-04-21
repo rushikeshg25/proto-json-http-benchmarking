aeb9e8e2571e6c409fb1d0615a20ad02a528999e
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-04-22 01:25:50 +0530

    feat: benchmarking both with net/http

---
+++ server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"google.golang.org/protobuf/proto"
+)
+
+type UserJSON struct {
+	Id       int32   `json:"id"`
+	Name     string  `json:"name"`
+	Email    string  `json:"email"`
+	IsActive bool    `json:"isActive"`
+	Score    float32 `json:"score"`
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	var user UserJSON
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
+}
+
+func protoHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	body, _ := io.ReadAll(r.Body)
+	err := proto.Unmarshal(body, &user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+	data, _ := proto.Marshal(&user)
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+
+}
+
+func main() {
+	http.HandleFunc("/json", jsonHandler)
+	http.HandleFunc("/proto", protoHandler)
+	log.Println("Listening on Port :8080")
+	http.ListenAndServe(":8080", nil)
+}
